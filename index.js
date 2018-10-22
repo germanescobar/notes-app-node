ff941b8938eef4b563caaa7697a17561372c2641
Germán Escobar <german.escobarc@gmail.com>
2018-10-22 11:29:38 -0500

    Explain routes

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const app = express();
 
+app.use("/static", express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hola Mundo</h1>");
+  const name = req.query.name;
+  const age = req.query.age;
+
+  res.send(`<h1>Hola ${name}, tienes ${age} años</h1>`);
 });
+
+app.get("/users/:name", (req, res) => {
+  const name = req.params.name;
+  res.send(`<h1>Hola ${name}</h1>`);
+})
+
+app.post("/users", (req, res) => {
+  res.status(404)
+  res.set("Content-Type", "text/plain");
+  res.send("No se encontró el recurso");
+});
+
 
 app.listen(3000, () => console.log("Listening on port 3000 ..."));
