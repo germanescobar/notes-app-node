267b33401d74e9410c5aa67a35823c3207a90d54
Germán Escobar <german.escobarc@gmail.com>
2019-01-03 17:27:16 -0500

    Use session to store notes

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const express = require("express");
 const cookieSession = require('cookie-session')
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log("Nueva petición HTTP");
-  next();
-};
-
 app.set("view engine", "pug");
 app.set("views", "views");
 app.use(express.urlencoded({ extended: true }));
@@ -15,43 +10,26 @@ app.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000
 }));
 app.use("/static", express.static("public"));
-app.use(logger);
 
+// muestra la lista de notas
 app.get("/", (req, res) => {
-  const name = req.query.name;
-  const age = req.query.age;
-
-  req.session.views = (req.session.views || 0) + 1
-
-  const notes = [
-    "Nota 1", "Nota 2", "Nota 3"
-  ];
-  res.render("index", { notes, views: req.session.views });
+  const notes = req.session.notes || [];
+  res.render("index", { notes });
 });
 
+// muestra el formulario para crear una nota
 app.get("/notes/new", (req, res) => {
   res.render("new");
 });
 
+// permite crear una nota
 app.post("/notes", (req, res) => {
-  console.log(req.body);
+  req.session.id = (req.session.id || 0) + 1;
+  const id = req.session.id
+
+  req.session.notes = req.session.notes || [];
+  req.session.notes.push({ id: id, title: req.body.title, body: req.body.body });
   res.redirect("/");
-});
-
-app.get("/users/:name", (req, res) => {
-  const name = req.params.name;
-  res.send(`<h1>Hola ${name}</h1>`);
-})
-
-app.post("/users", (req, res) => {
-  res.status(404);
-  res.set("Content-Type", "text/plain");
-  res.send("No se encontró el recurso");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Algo salió mal");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000 ..."));
