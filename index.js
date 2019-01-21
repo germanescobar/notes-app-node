d0084d1934338381ce8ae7976a83e4cb57e11200
Germán Escobar <german.escobarc@gmail.com>
2019-01-21 10:44:29 -0500

    Implement show and add styles

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cookieSession({
   secret: "una_cadena_secreta",
   maxAge: 24 * 60 * 60 * 1000
 }));
-app.use("/static", express.static("public"));
+app.use("/assets", express.static("assets"));
 
 // muestra la lista de notas
 app.get("/", async (req, res) => {
@@ -22,8 +22,9 @@ app.get("/", async (req, res) => {
 });
 
 // muestra el formulario para crear una nota
-app.get("/notes/new", (req, res) => {
-  res.render("new");
+app.get("/notes/new", async (req, res) => {
+  const notes = await Note.find();
+  res.render("new", { notes });
 });
 
 // permite crear una nota
@@ -41,6 +42,12 @@ app.post("/notes", async (req, res, next) => {
   }
 
   res.redirect("/");
+});
+
+app.get("/notes/:id", async (req, res) => {
+  const notes = await Note.find();
+  const note = await Note.findById(req.params.id);
+  res.render("show", { notes: notes, currentNote: note });
 });
 
 app.listen(3000, () => console.log("Listening on port 3000 ..."));
--- models/Note.js
+++ models/Note.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const NoteSchema = mongoose.Schema({
-  title: String,
+  title: { type: String, required: true },
   body: String
 });
+NoteSchema.methods.truncateBody = function() {
+  if (this.body && this.body.length > 75) {
+    return this.body.substring(0, 70) + " ...";
+  }
+  return this.body;
+};
 const Note = mongoose.model("Note", NoteSchema);
 module.exports = Note;
