100d7ad853946d59cb950f22336219552482527e
Germán Escobar <german.escobarc@gmail.com>
2019-05-21 09:20:56 -0500

    Prepare app for Heroku deployment

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const Note = require("./models/Note");
 const User = require("./models/User");
 const cookieSession = require("cookie-session")
 const md = require("marked");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/notes", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/notes", { useNewUrlParser: true });
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -155,5 +156,4 @@ app.get("/logout", requireUser, (req, res) => {
   res.redirect("/login");
 });
 
-
-app.listen(3000, () => console.log("Listening on port 3000 ..."));
+app.listen(PORT, () => console.log(`Listening on port ${PORT} ...`));
